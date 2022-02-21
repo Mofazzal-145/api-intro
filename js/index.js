a4a159b8525809d4cb94f90ab524397981059afe
Mofazzal-145 <md.mofazzalhossain145@gmail.com>
2022-02-22 02:10:10 +0600

    JSON(stringify & parse)

--- js/index.js
+++ js/index.js
@@ -1,4 +1,29 @@
 
+// JavaScript Object Notation
+// JSON
 const user = {id: 32, name: 'mofazzal', job: 'student'};
-
+const stringified = JSON.stringify(user);
+/* 
 console.log(user);
+console.log(stringified);
+ */
+
+const shop = {
+    name: 'Afzal store',
+    address: 'washa road',
+    profit: 13000,
+    product: ['laptop', 'mobile', 'pepsi'],
+    owner: {
+        name: 'hasan ali',
+        profession: 'everything',
+    },
+    isExpensive: true
+}
+
+const shopStringified = JSON.stringify(shop);
+// console.log(shop);
+console.log(shopStringified);
+
+const converted = JSON.parse(shopStringified);
+console.log(converted);
+
